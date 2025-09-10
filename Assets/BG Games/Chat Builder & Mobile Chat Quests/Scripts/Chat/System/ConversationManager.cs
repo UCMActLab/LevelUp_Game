72686118e51821bfe1dbdba985e396d2ec89f234
Alejandro Romero <arome90@users.noreply.github.com>
2025-09-10 14:54:17 -0400

    Primeras pantallas montadas

--- Assets/BG Games/Chat Builder & Mobile Chat Quests/Scripts/Chat/System/ConversationManager.cs
+++ Assets/BG Games/Chat Builder & Mobile Chat Quests/Scripts/Chat/System/ConversationManager.cs
@@ -8,7 +8,6 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
