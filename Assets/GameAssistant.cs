81649efc80fc7b5f0580d28b5976496087c8e53a
DavidRainder <davirive@ucm.es>
2026-01-07 21:27:51 +0100

    arreglos en chatdemo

---
+++ Assets/GameAssistant.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameAssistant : MonoBehaviour
+{
+    /// Te suelta la chapa: El avatar detecta que el jugador NO está interactuando con la pantalla. 
+    ///     Esto quiere decir que el jugador no ha tocado ningún botón y ni siquiera está haciendo scroll.
+    ///     En caso de SÍ estar haciendo scroll, podemos poner más tiempo para decirle al juego que el jugador todavía
+    ///     no está perdido. Si finalmente pasa X + Y tiempo y solo ha hecho scroll, se le avisa de la siguiente interacción posible
+    
+    /// RESUMEN: Avisar al jugador de la siguiente interaccióntras X + Y tiempo, siendo X un valor serialiazdo e 
+    ///     Y = 0 si no ha hecho scroll o 
+    ///     Y = Z si sí ha hecho scroll (siendo Z otro valor configurable)
+   
+    /// Te pide interacción: Cuando el avatar detecte que el jugador no lo está haciendo muy bien 
+    ///     (que no lo hace perfecto durante X artículos o más) este mostrará una exclamación y se verá preocupado. 
+    ///     Cuando se interactúe con él, se le soltará la chapa acerca de la importancia de leer y compartir buenos artículos
+   
+    /// este nota tiene que tener acceso a la puntuación del nivel actual, así que necesitará referencias al LevelManager y habrá que hacer públicas varias cosas
+    /// también tiene que ver cuántas interaccione estás haciendo con la pantalla
+    /// también tiene que ver qué botones ya has pulsado (si has leído el artículo, si lo has compartido...)
+    /// va a necesitar diferentes estados:
+    ///     - NORMAL: Sonríe feliz viendo que el jugador ha hecho bien 1 nivel (este es el estado base)
+    ///     - WORRIED: Detecta que el jugador no está haciéndolo muy bien y se pone sad. Aquí es cuándo debemos lanzar la interrogación
+    ///     - SURPRISED: Está súper contento porque ve que el jugador lleva haciéndolo bien 2 o más niveles
+
+}
